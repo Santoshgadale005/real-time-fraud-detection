8b3737c5e5065509ddddf33d05b48eab0fadb9f9
Bharath Reddy Voodem <voodembharath@gmail.com>
2026-07-15 18:17:02 +0530

    Day 20: Integrate Prometheus monitoring with FastAPI metrics

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+
 from app.api.predict import router as predict_router
 from app.api.health import router as health_router
 from app.api.metadata import router as metadata_router
+
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(
     title="Real-Time Fraud Detection API",
@@ -19,3 +22,6 @@ def home():
 app.include_router(predict_router)
 app.include_router(health_router)
 app.include_router(metadata_router)
+
+# Prometheus Metrics
+Instrumentator().instrument(app).expose(app)
